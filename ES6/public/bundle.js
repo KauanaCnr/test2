7944cde2c3a7f6d550e8b946eb06d91032b93121
KauanaCnr <kauanacavalheiro2001@gmail.com>
2021-09-28 17:27:26 -0300

    Dia 27-09 part 4

--- ES6/public/bundle.js
+++ ES6/public/bundle.js
@@ -1,103 +1,232 @@
-//const and let *//
-//*const person = {
-//   name:'Youg'
-//}
-//person.name = 'Oliver'
-//console.log(person)
-//-----------------
-//for(var i = 0;i <10; i++){
-//  console.log(i)
-//}
-//*Array*//
-//const numbers = [1,2,3,4,5]
-//const newNumbers = numbers.map(function(number){
-// return number + 1
-//}
-//)
-//console.log(newNumbers)
-//console.log(numbers)
-//-----------------------------
-//const numbers = [1, 2, 3, 4, 5]
-//const total = numbers.reduce(function(total, next){
-// return total + next
-//})
-//console.log(total)
-//------------------------------------
-//const todos = [
-//{text: 'lavar a louça', done:false},
-//{text: 'apanhar o lixo', done:true},
-//{text: 'dobrar as roupas', done:true},
-//]
-//const doneTodos = todos.filter(function(todo){
-//  return todo.done 
-//})
-//console.log(doneTodos)
-//------------------------
-//const todos = [
-//{text: 'lavar a louça', done:false},
-//{text: 'apanhar o lixo', done:true},
-//{text: 'dobrar as roupas', done:true},
-//]
-//const final = todos.find(function(todo){
-//  return todo.text == 'dobrar as roupas'
-//})
-//console.log(final)
-//------------------------
-//const todos = [
-// {text: 'lavar a louça', done:false},
-//{text: 'apanhar o lixo', done:true},
-//{text: 'dobrar as roupas', done:true},
-//]
-//const index = todos.findIndex(function(todo){
-//  return todo.text == 'lavar a louça'
-//})
-//console.log(index)
-//-------------------------
-//const cars =['fusca', 'civic', 'hb20']
-//const hasFusca = cars.some(function(car) {
-//return car == 'fusca'
-//})
-//console.log(hasFusca)
-//------------
-//*Arrow Functions
-//const todos = [
-//{text: 'lavar a louça', done:false},
-//{text: 'apanhar o lixo', done:true},
-//{text: 'dobrar as roupas', done:true},
-//]
-//const doneTodos = todos.filter((todo) => todo.done 
-//)
-//console.log(doneTodos)
-//---------------------
-//function anything ()  {
-//  console.log('Hello world')
-//}
-//---------------------------------
-//const anything  = ()  => ({mensagem:'Hello world'})
-//console.log(anything())
-//*Parametro padrao
-//function  sayHello(name = 'Fulano')  {
-//console.log('Olá' + name)
-//}
-//-------------------
-//const sayHello  = (name = 'Fulano')  => console.log('Olá' + name)
-//sayHello()
-//-----------------------------------
-//*Template strings
-//const showProduct = (product, price) => {
-//console.log(`O valor de ${product} é R$ ${price} pode comprar`)
-//}
-//showProduct('camisa', 20.7)
-//------------------------
-//*Object Short Syntax
-//const name = 'camisa'
-//const price = 20
-//const product = {
-//name,
-//price,
-//inStock: true
-//}
-//console.log(product)
-//-----------------------------------
-//*Desestruturação
 "use strict";
+
+/*const and let *
+
+const person = {
+    name:'Youg'
+}
+
+person.name = 'Oliver'
+
+console.log(person)
+-----------------
+
+for(var i = 0;i <10; i++){
+    console.log(i)
+}
+
+//*Array*
+
+const numbers = [1,2,3,4,5]
+
+const newNumbers = numbers.map(function(number){
+     return number + 1
+}
+)
+
+console.log(newNumbers)
+console.log(numbers)
+-----------------------------
+const numbers = [1, 2, 3, 4, 5]
+
+const total = numbers.reduce(function(total, next){
+  return total + next
+
+})
+
+console.log(total)
+------------------------------------
+
+const todos = [
+  {text: 'lavar a louça', done:false},
+  {text: 'apanhar o lixo', done:true},
+  {text: 'dobrar as roupas', done:true},
+]
+
+const doneTodos = todos.filter(function(todo){
+    return todo.done 
+})
+
+console.log(doneTodos)
+
+------------------------
+
+const todos = [
+  {text: 'lavar a louça', done:false},
+  {text: 'apanhar o lixo', done:true},
+  {text: 'dobrar as roupas', done:true},
+]
+
+const final = todos.find(function(todo){
+    return todo.text == 'dobrar as roupas'
+})
+
+console.log(final)
+------------------------
+const todos = [
+  {text: 'lavar a louça', done:false},
+  {text: 'apanhar o lixo', done:true},
+  {text: 'dobrar as roupas', done:true},
+]
+
+const index = todos.findIndex(function(todo){
+    return todo.text == 'lavar a louça'
+})
+
+console.log(index)
+
+-------------------------
+
+const cars =['fusca', 'civic', 'hb20']
+
+const hasFusca = cars.some(function(car) {
+    //return car == 'fusca'
+})
+
+console.log(hasFusca)
+
+---------------
+//*Arrow Functions
+
+const todos = [
+  {text: 'lavar a louça', done:false},
+  {text: 'apanhar o lixo', done:true},
+  {text: 'dobrar as roupas', done:true},
+]
+
+const doneTodos = todos.filter((todo) => todo.done 
+)
+
+console.log(doneTodos)
+
+---------------------
+
+function anything ()  {
+    console.log('Hello world')
+}
+
+---------------------------------
+const anything  = ()  => ({mensagem:'Hello world'})
+
+console.log(anything())
+
+//*Parametro padrao
+
+function  sayHello(name = 'Fulano')  {
+    console.log('Olá' + name)
+}
+
+-------------------
+
+const sayHello  = (name = 'Fulano')  => console.log('Olá' + name)
+
+sayHello()
+
+-----------------------------------
+
+//*Template strings
+
+const showProduct = (product, price) => {
+console.log(`O valor de ${product} é R$ ${price} pode comprar`)
+}
+
+showProduct('camisa', 20.7)
+
+------------------------
+
+//*Object Short Syntax
+
+const name = 'camisa'
+const price = 20
+
+const product = {
+    name,
+    price,
+    inStock: true
+}
+
+console.log(product)
+
+-----------------------------------
+
+//*Desestruturação
+
+const fruits = ['laranja', 'morango', 'maçã']
+
+const [a, c]  = fruits
+
+console.log(a, c)
+
+--------------------
+
+const person = {
+  firstName: 'Yung',
+  secondName: 'Silva',
+  age: 23,
+  address: {
+    city: 'Recife',
+    region: 'PE'
+  }
+}
+
+const {firstName,  secondName,  address:  {city}}  = person
+
+console.log(firstName,  secondName, city)
+
+------------------------
+
+const person = {
+  firstName: 'Yung',
+  secondName: 'Silva',
+  age: 23,
+  address: {
+    city: 'Recife',
+    region: 'PE'
+  }
+}
+
+const showFullName = ({firstName, secondName}) =>  {
+  console.log(`${firstName} ${secondName}`)
+
+}
+
+showFullName(person)
+
+--------------------------
+///*rest and spread
+
+rest
+
+const numbers = [1, 2, 3, 4, 5]
+
+const [first, second, ...rest] = numbers 
+
+console.log(rest)
+
+-------------------------------
+
+const person = {
+  name: 'Yung',
+  age: 23,
+  height: 1.73,
+  company: 'Catalina',
+}
+
+const {name, ...rest}  = person
+
+console.log(rest)
+
+---------------------------------
+
+*/
+var sum = function sum() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
+};
+
+console.log(sum(5, 5, 10, 10));
--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,151 +1,228 @@
-//const and let *//
+/*const and let *
 
-//*const person = {
- //   name:'Youg'
-//}
+const person = {
+    name:'Youg'
+}
 
-//person.name = 'Oliver'
+person.name = 'Oliver'
 
-//console.log(person)
-//-----------------
+console.log(person)
+-----------------
 
-//for(var i = 0;i <10; i++){
-  //  console.log(i)
-//}
+for(var i = 0;i <10; i++){
+    console.log(i)
+}
 
-//*Array*//
+//*Array*
 
-//const numbers = [1,2,3,4,5]
+const numbers = [1,2,3,4,5]
 
-//const newNumbers = numbers.map(function(number){
-    // return number + 1
-//}
-//)
+const newNumbers = numbers.map(function(number){
+     return number + 1
+}
+)
 
-//console.log(newNumbers)
-//console.log(numbers)
-//-----------------------------
-//const numbers = [1, 2, 3, 4, 5]
+console.log(newNumbers)
+console.log(numbers)
+-----------------------------
+const numbers = [1, 2, 3, 4, 5]
 
-//const total = numbers.reduce(function(total, next){
- // return total + next
+const total = numbers.reduce(function(total, next){
+  return total + next
 
-//})
+})
 
-//console.log(total)
-//------------------------------------
+console.log(total)
+------------------------------------
 
-//const todos = [
-  //{text: 'lavar a louça', done:false},
-  //{text: 'apanhar o lixo', done:true},
-  //{text: 'dobrar as roupas', done:true},
-//]
+const todos = [
+  {text: 'lavar a louça', done:false},
+  {text: 'apanhar o lixo', done:true},
+  {text: 'dobrar as roupas', done:true},
+]
 
-//const doneTodos = todos.filter(function(todo){
-  //  return todo.done 
-//})
+const doneTodos = todos.filter(function(todo){
+    return todo.done 
+})
 
-//console.log(doneTodos)
+console.log(doneTodos)
 
-//------------------------
+------------------------
 
-//const todos = [
-  //{text: 'lavar a louça', done:false},
-  //{text: 'apanhar o lixo', done:true},
-  //{text: 'dobrar as roupas', done:true},
-//]
+const todos = [
+  {text: 'lavar a louça', done:false},
+  {text: 'apanhar o lixo', done:true},
+  {text: 'dobrar as roupas', done:true},
+]
 
-//const final = todos.find(function(todo){
-  //  return todo.text == 'dobrar as roupas'
-//})
+const final = todos.find(function(todo){
+    return todo.text == 'dobrar as roupas'
+})
 
-//console.log(final)
-//------------------------
-//const todos = [
- // {text: 'lavar a louça', done:false},
-  //{text: 'apanhar o lixo', done:true},
-  //{text: 'dobrar as roupas', done:true},
-//]
+console.log(final)
+------------------------
+const todos = [
+  {text: 'lavar a louça', done:false},
+  {text: 'apanhar o lixo', done:true},
+  {text: 'dobrar as roupas', done:true},
+]
 
-//const index = todos.findIndex(function(todo){
-  //  return todo.text == 'lavar a louça'
-//})
+const index = todos.findIndex(function(todo){
+    return todo.text == 'lavar a louça'
+})
 
-//console.log(index)
+console.log(index)
 
-//-------------------------
+-------------------------
 
-//const cars =['fusca', 'civic', 'hb20']
+const cars =['fusca', 'civic', 'hb20']
 
-//const hasFusca = cars.some(function(car) {
+const hasFusca = cars.some(function(car) {
     //return car == 'fusca'
-//})
+})
 
-//console.log(hasFusca)
+console.log(hasFusca)
 
-//------------
+---------------
 //*Arrow Functions
 
-//const todos = [
-  //{text: 'lavar a louça', done:false},
-  //{text: 'apanhar o lixo', done:true},
-  //{text: 'dobrar as roupas', done:true},
-//]
+const todos = [
+  {text: 'lavar a louça', done:false},
+  {text: 'apanhar o lixo', done:true},
+  {text: 'dobrar as roupas', done:true},
+]
 
-//const doneTodos = todos.filter((todo) => todo.done 
-//)
+const doneTodos = todos.filter((todo) => todo.done 
+)
 
-//console.log(doneTodos)
+console.log(doneTodos)
 
-//---------------------
+---------------------
 
-//function anything ()  {
-  //  console.log('Hello world')
-//}
+function anything ()  {
+    console.log('Hello world')
+}
 
-//---------------------------------
-//const anything  = ()  => ({mensagem:'Hello world'})
+---------------------------------
+const anything  = ()  => ({mensagem:'Hello world'})
 
-//console.log(anything())
+console.log(anything())
 
 //*Parametro padrao
 
-//function  sayHello(name = 'Fulano')  {
-    //console.log('Olá' + name)
-//}
+function  sayHello(name = 'Fulano')  {
+    console.log('Olá' + name)
+}
 
-//-------------------
+-------------------
 
-//const sayHello  = (name = 'Fulano')  => console.log('Olá' + name)
+const sayHello  = (name = 'Fulano')  => console.log('Olá' + name)
 
-//sayHello()
+sayHello()
 
-//-----------------------------------
+-----------------------------------
 
 //*Template strings
 
-//const showProduct = (product, price) => {
-//console.log(`O valor de ${product} é R$ ${price} pode comprar`)
-//}
+const showProduct = (product, price) => {
+console.log(`O valor de ${product} é R$ ${price} pode comprar`)
+}
 
-//showProduct('camisa', 20.7)
+showProduct('camisa', 20.7)
 
-//------------------------
+------------------------
 
 //*Object Short Syntax
 
-//const name = 'camisa'
-//const price = 20
+const name = 'camisa'
+const price = 20
 
-//const product = {
-    //name,
-    //price,
-    //inStock: true
-//}
+const product = {
+    name,
+    price,
+    inStock: true
+}
 
-//console.log(product)
+console.log(product)
 
-//-----------------------------------
+-----------------------------------
 
 //*Desestruturação
+
+const fruits = ['laranja', 'morango', 'maçã']
+
+const [a, c]  = fruits
+
+console.log(a, c)
+
+--------------------
+
+const person = {
+  firstName: 'Yung',
+  secondName: 'Silva',
+  age: 23,
+  address: {
+    city: 'Recife',
+    region: 'PE'
+  }
+}
+
+const {firstName,  secondName,  address:  {city}}  = person
+
+console.log(firstName,  secondName, city)
+
+------------------------
+
+const person = {
+  firstName: 'Yung',
+  secondName: 'Silva',
+  age: 23,
+  address: {
+    city: 'Recife',
+    region: 'PE'
+  }
+}
+
+const showFullName = ({firstName, secondName}) =>  {
+  console.log(`${firstName} ${secondName}`)
+
+}
+
+showFullName(person)
+
+--------------------------
+///*rest and spread
+
+/rest/
+
+const numbers = [1, 2, 3, 4, 5]
+
+const [first, second, ...rest] = numbers 
+
+console.log(rest)
+
+-------------------------------
+
+const person = {
+  name: 'Yung',
+  age: 23,
+  height: 1.73,
+  company: 'Catalina',
+}
+
+const {name, ...rest}  = person
+
+console.log(rest)
+
+---------------------------------
+
+*/
+
+const sum = (...params)  =>{
+  return  params.reduce((total, next) => total  + next)
+}
+
+console.log(sum(5, 5, 10, 10))
+
+
 
